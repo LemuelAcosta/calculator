51bced3cf7b6f5781ebaccf3c3f4450bbcadefc4
Lemuel <63744340+LemuelAcosta@users.noreply.github.com>
2025-05-12 17:54:29 -0400

    create listener for numbers and operators

--- js.js
+++ js.js
@@ -1,3 +1,29 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operator");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        firstNumber = button.textContent.trim()
+        console.log("first number: " + firstNumber);
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        operator = button.textContent.trim();
+        console.log("Operator: " + operator);
+    })
+})
+
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", () => {
+    console.log("equaling")
+})
+
 function add(a, b) {
     return a + b
 }
@@ -12,4 +38,8 @@ function multiply(a, b) {
 
 function  divide(a, b) {
     return  b / a
+}
+
+function operate(firstNumber, operator, secondNumber) {
+
 }
